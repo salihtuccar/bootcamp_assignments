9ea0458da080637788559b665afbaf38609ad8b8
salihtuccar <salihtuccar@Salih-MacBook-Air.local>
2022-03-30 17:49:22 +0300

    negative_meaning.py created and added

---
+++ negative_meaning.py
@@ -0,0 +1,13 @@
+# Define a function to take a word and return negative meaning.
+# Given a word, return a new word where "not " has been added to the front. 
+# However, if the word already begins with "not", return the string unchanged.
+
+
+def not_string(word):
+    if word[:3] == 'not' :
+        return word
+    else:
+        return "not " + word
+        
+    
+
